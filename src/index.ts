1d632178a68d29d586a541041e8fa0ba10041f06
Philip Trachtenberg <philtrachtenberg@gmail.com>
2022-09-26 11:33:14 +0200

    removed unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/* import { v4 as uuidv4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+console.log(uuidv4()) */
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.getElementById('new-task-form') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+
+console.log(list)
+console.log(form)
+console.log(input)
